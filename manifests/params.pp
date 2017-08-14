8ee09e74decf1b13c396e8d5efbaebf1f309dec1
Chad Quilter <chadquilter@hotmail.com>
2017-08-14 11:15:47 -0500

    add new updates and configs

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,6 @@
+## Only for on Centos 7
 class dnsmasq::params {
   case $::osfamily {
-    'Debian': {
-      $package_name = 'dnsmasq'
-      $service_name = 'dnsmasq'
-      $config_file = '/etc/dnsmasq.conf'
-      $resolv_file = '/etc/resolv.conf.dnsmasq'
-      $config_dir = '/etc/dnsmasq.d/'
-    }
     'RedHat': {
       $package_name = 'dnsmasq'
       $service_name = 'dnsmasq'
