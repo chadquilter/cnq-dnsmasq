eba472f12b3c5d82544a7ebe9e44bcb60ee1febc
Chad Quilter <chadquilter@hotmail.com>
2017-08-14 04:06:39 -0500

    add new updates and configs

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -8,7 +8,10 @@ define dnsmasq::conf (
 
   file { "${dnsmasq::params::config_dir}${prio}-${name}":
     ensure  => $ensure,
+    owner   => 'root',
+    group   => 'root',
     content => $content,
     source  => $source,
-    notify  => Class['dnsmasq::service']
+    notify  => Class['dnsmasq::service'],
+  }
 }
